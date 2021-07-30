b0c74df4560eb55441efabea7442931d57cbdbbe
Anchit Kalra <53076375+AnchitKalra@users.noreply.github.com>
2021-07-30 17:38:24 +0530

    bug fix snackbar added

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -6,11 +6,30 @@ import { FormGroup } from "@material-ui/core";
 import { InputLabel } from "@material-ui/core";
 import { FormControl } from "@material-ui/core";
 import { Button } from "@material-ui/core";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
 
 class LoginPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showMessage: false,
+      successMessage: "",
+    };
+  }
+
+  handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    this.setState({ showMessage: false });
+  };
+
   submitHandler = (e) => {
     let b = this;
     e.preventDefault();
+    let that = this;
     let details = e.target;
     let xhr = new XMLHttpRequest();
     let enc = details.number.value + ":" + details.password.value;
@@ -23,7 +42,10 @@ class LoginPage extends Component {
         sessionStorage.clear();
         let a = JSON.parse(this.response);
         if (a.status === 500) {
-          alert("Please enter correct Info");
+          that.setState({
+            showMessage: true,
+            successMessage: "Please enter correct info",
+          });
         } else {
           sessionStorage.setItem(
             "access-token",
@@ -72,7 +94,7 @@ class LoginPage extends Component {
                   type="password"
                 />
                 <FormHelperText id="my-helper-text">
-                  Please Fill a strong password
+                  Please fill the password
                 </FormHelperText>
               </FormControl>
               <FormControl className="formcontrol">
@@ -82,6 +104,29 @@ class LoginPage extends Component {
               </FormControl>
             </FormGroup>
           </form>
+          <Snackbar
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            open={this.state.showMessage}
+            onClose={this.handleClose}
+            autoHideDuration={6000}
+            ContentProps={{
+              "aria-describedby": "message-id",
+            }}
+            message={<span id="message-id"> {this.state.successMessage}</span>}
+            action={[
+              <IconButton
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                onClick={this.handleClose}
+              >
+                <CloseIcon />
+              </IconButton>,
+            ]}
+          />
         </div>
       </div>
     );
